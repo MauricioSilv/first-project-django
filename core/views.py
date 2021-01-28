e2e1772936f1975aa4661d54e221f288067cfb41
mauriciosilv <mausilva828@gmail.com>
2021-01-28 17:42:56 -0300

    templates no django

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'opiniao': 'Django é massa!'
+    }
+    return render(request, 'index.html', context)
+
 
 def contato(request):
     return render(request, 'contato.html')
