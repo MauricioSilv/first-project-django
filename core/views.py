e8db49db8a5442a222b114ce4db02050e2885291
mauriciosilv <mausilva828@gmail.com>
2021-01-28 20:23:55 -0300

    trazendo e exibindo dados no template

--- core/views.py
+++ core/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 
 def index(request):
+    produtos = Produto.objects.all()
     context = {
-        'opiniao': 'Django é massa!'
+        'opiniao': 'Django é massa!',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    products = Produto.objects.get(id=pk)
+    context = {
+        'produto': products
+    }
+    return render(request, 'produto.html', context)
